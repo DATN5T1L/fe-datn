c9827769e208e10c9980592b29fea7ac55616956
DATN5T1L <polypage.project@gmail.com>
2024-09-19 17:57:51 +0700

    thay đổi bởi thuận

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import styles from '@public/styles/Register/Register.module.css'
+import styles from '@public/styles/register/Register.module.css'
 import Link from 'next/link';
 import { Button, Card, Container, Form, Image } from 'react-bootstrap';
 
