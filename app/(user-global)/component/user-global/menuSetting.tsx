6bec9cbc598f7e249b041b8561522d630dfa66b7
thuan01112003 <thuan.01112003@gmail.com>
2024-10-12 16:26:02 +0700

    UI User - 12/10/2024 - thuận đỗ

--- app/(user-global)/component/user-global/menuSetting.tsx
+++ app/(user-global)/component/user-global/menuSetting.tsx
@@ -60,7 +60,6 @@ const MenuSetting: React.FC = () => {
                 show={showChangePassWord}
                 onClose={() => setShowChangePassWord(false)}
             />
-            <div style={{display:'none'}}></div>
         </>
     )
 }
