ba66bc59b7f5dfc2c897a2590bcb3b0ffc9dd71e
tuanhuynh0107 <ht24430@gmaile.com>
2024-10-16 17:03:03 +0700

    cập nhật lần n

--- app/(user-global)/coursefor/page.tsx
+++ app/(user-global)/coursefor/page.tsx
@@ -1,58 +1,54 @@
-'use client'
+'use client';
 
 import Body from "../component/globalControl/body";
 import CourseFor from "./CourseFor/page";
 import CourseForNext from "./CourseForNext/page";
-import TimeLine from "../component/router/timeLine"
-import LearningPathSection from "../component/router/learningPathSection"
+import TimeLine from "../component/router/timeLine";
+import LearningPathSection from "../component/router/learningPathSection";
 
 
-const courseForYou: React.FC = () => {
+
+const CourseForYou: React.FC = () => {
     return (
-        <>
-            <title>TTO - Khóa học của bạn</title>
-            <meta name="description" content="Được tạo bởi Taem TTO" />
-            <Body>
-                <CourseFor />
-                <CourseForNext />
-                <LearningPathSection
-                    title='UI/UX Design'
-                    contentTitle='Thiết kế UI/UX luôn là một lĩnh vực hấp dẫn và thời thượng.
-                     Chỉ cần tìm kiếm từ khóa "Tuyển dụng Designer UI/UX,"
+        <Body>
+            <CourseFor />
+            <CourseForNext />
+            <LearningPathSection
+                title='UI/UX Design'
+                contentTitle='Thiết kế UI/UX luôn là một lĩnh vực hấp dẫn và thời thượng.
+                     Chỉ cần tìm kiếm từ khóa "Tuyển dụng Designer UI/UX," 
                       bạn sẽ ngay lập tức nhận thấy các nhà tuyển dụng đang
                        tích cực tìm kiếm ứng viên với mức lương rất cạnh tranh.
                         Nếu bạn có đam mê với việc tạo ra những giao diện người dùng độc đáo và sáng tạo,
                          thì việc trau dồi kỹ năng thiết kế UI/UX là điều cần thiết.
                      Hãy để khóa học này giúp bạn phát triển và hiện thực hóa những ý tưởng của mình!'
-                    contentSkill='Sau khi hoàn thành lộ trình học này,
+                contentSkill='Sau khi hoàn thành lộ trình học này,
                      bạn sẽ có khả năng tạo ra các sản phẩm thiết kế UI/UX hiện đại và thu hút,
                       từ những giao diện cơ bản cho đến các nguyên mẫu phức tạp,
                      cùng với kỹ năng tối ưu hóa trải nghiệm người dùng.'
-                    imgLearningPath='/img/learningPathDS.svg'
-                    webImg="/img/webDS.svg"
-                    icon1="/img/adobeIcon.svg"
-                    icon2="/img/figmaIcon.svg"
-                    icon3="/img/fireBaseIcon.svg"
-                    icon4="/img/reactIcon.svg"
-                    icon5="/img/psIcon.svg"
-                    content1="Nền tảng thiết kế"
-                    content2="Thiết kế giao diện cơ bản"
-                    content3="Giao diện phức tạp và trải nghiệm người dùng nâng cao."
-                    content4="Nguyên mẫu tương tác"
-                    content5="Đánh giá và cải thiện trải nghiệm người dùng"
-                />
-                <TimeLine
-                    title="UI/UX DESIGNER"
-                    name1="Introduction to Design"
-                    name2="Advanced UI/UX Design"
-                    name3="Prototyping with Figma "
-                    name4="MySQL"
-                    name5="Usability Testing"
-                ></TimeLine>
-
-            </Body>
-        </>
-    )
+                imgLearningPath='/img/learningPathDS.svg'
+                webImg="/img/webDS.svg"
+                icon1="/img/adobeIcon.svg"
+                icon2="/img/figmaIcon.svg"
+                icon3="/img/fireBaseIcon.svg"
+                icon4="/img/reactIcon.svg"
+                icon5="/img/psIcon.svg"
+                content1="Nền tảng thiết kế"
+                content2="Thiết kế giao diện cơ bản"
+                content3="Giao diện phức tạp và trải nghiệm người dùng nâng cao."
+                content4="Nguyên mẫu tương tác"
+                content5="Đánh giá và cải thiện trải nghiệm người dùng"
+            />
+            <TimeLine
+                title="UI/UX DESIGNER"
+                name1="Introduction to Design"
+                name2="Advanced UI/UX Design"
+                name3="Prototyping with Figma "
+                name4="MySQL"
+                name5="Usability Testing"
+            />
+        </Body>
+    );
 }
 
-export default courseForYou;
+export default CourseForYou;
