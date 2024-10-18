81723c4ccde88c6257861d8d617d98174865341b
tuanhuynh0107 <ht24430@gmaile.com>
2024-10-18 21:39:52 +0700

    cap nhat code

--- app/auth.ts
+++ app/auth.ts
@@ -1,10 +1,18 @@
-
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import Facebook from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google, Facebook],
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+    ],
     callbacks: {
         async jwt({ token, account }) {
             if (account) {
