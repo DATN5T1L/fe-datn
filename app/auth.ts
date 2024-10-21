be50af1099ebe4b81dc266606818f415b2f36e5e
thuan01112003 <thuan.01112003@gmail.com>
2024-10-21 22:49:50 +0700

    fix bug - 21/10/2024 - By Thuận Đỗ

--- app/auth.ts
+++ app/auth.ts
@@ -2,17 +2,8 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
 
-export default NextAuth({
-    providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-        Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        }),
-    ],
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [Google, Facebook],
     callbacks: {
         async jwt({ token, account }) {
             if (account) {
