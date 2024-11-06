e06cd606acdae98cb81cacddf9fe544d37fc55be
thaotran-183 <tranhieuthao180303@gmail.com>
2024-11-06 17:54:06 +0700

    thay doi boi Thao

--- app/(user-global)/component/globalControl/HeroPost.tsx
+++ app/(user-global)/component/globalControl/HeroPost.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import React from 'react';
-import { Container, Row, Col, Nav, Image, Button, } from 'react-bootstrap';
-
-// styles
 import styles from "@public/styles/post/HeroPost.module.css";
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import styles from "@public/styles/post/post.module.css";
 
 const HeroPost: React.FC = () => {
     return (
@@ -39,42 +34,11 @@ const HeroPost: React.FC = () => {
                         <figcaption className={styles.descImg}><span className={styles.content}>Xu hướng công nghệ</span></figcaption>
                     </figure>
                 </Col>
-            </Row>
+            </Row>   {/* postMain  */}
+            <h3 className={styles.title2}>
+                Cùng xem qua những bài viết nổi bật của TTO
+            </h3>
         </Container>
-    return(
-        <>
-            <Container className={styles.container}>
-                <h2 className={styles.title}>
-                    Bản tin TTO.SH
-                </h2>
-                <Row className={styles.Wapper}>
-                    <Col xs={6} md={3}>
-                        <figure className={styles.itemList}>
-                            <Image className={styles.image} src="https://images.unsplash.com/photo-1719937206341-38a6392dfdef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                                <figcaption className={styles.descImg}>Xu hướng công nghệ</figcaption>
-                        </figure>
-                    </Col>
-                    <Col xs={6} md={3}>
-                        <figure className={styles.itemList}>
-                            <Image className={styles.image} src="https://images.unsplash.com/photo-1719937206341-38a6392dfdef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                                <figcaption className={styles.descImg}>Xu hướng công nghệ</figcaption>
-                        </figure>
-                    </Col>
-                    <Col xs={6} md={3}>
-                        <figure className={styles.itemList}>
-                            <Image className={styles.image} src="https://images.unsplash.com/photo-1719937206341-38a6392dfdef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                                <figcaption className={styles.descImg}>Xu hướng công nghệ</figcaption>
-                        </figure>
-                    </Col>
-                    <Col xs={6} md={3}>
-                        <figure className={styles.itemList}>
-                            <Image className={styles.image} src="https://images.unsplash.com/photo-1719937206341-38a6392dfdef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                                <figcaption className={styles.descImg}>Xu hướng công nghệ</figcaption>
-                        </figure>
-                    </Col>
-                </Row>
-            </Container>
-        </>
     )
 }
 
