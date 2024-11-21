bf64afd631197ed565f200d7595f060b1e6e3e21
tuanhuynh0107 <ht24430@gmaile.com>
2024-11-21 21:54:06 +0700

    課pンはt

---
+++ app/(user-global)/Contact/Content.tsx
@@ -0,0 +1,21 @@
+import { Container, Col, Row } from 'react-bootstrap';
+
+import c from '@public/styles/Contact/contact.module.css'
+const Hero = () => {
+    return (
+        <Container className={c.Container}>Hero</Container>
+    )
+}
+
+const Form = () => {
+    return (
+        <Container className={c.Container}>Contant</Container>
+    )
+}
+const Map = () => {
+    return (
+        <Container className={c.Container}>Contant</Container>
+    )
+}
+
+export { Hero, Form, Map };
---
+++ app/(user-global)/Contact/page.tsx
@@ -0,0 +1,13 @@
+import { Container, Col, Row } from 'react-bootstrap'
+
+import { Hero, Form } from './Content';
+import { Main } from 'next/document';
+const Contact = () => {
+    return (
+        <main>
+            <Hero />
+            <Form />
+        </main>
+    )
+}
+export default Contact;
