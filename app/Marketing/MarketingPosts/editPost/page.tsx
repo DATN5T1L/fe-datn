f12409bc7026f12427779cbe15190027a5a9c264
thuan01112003 <139634561+lag0111@users.noreply.github.com>
2024-12-11 01:13:42 +0700

    fix bug vecel

--- app/Marketing/MarketingPosts/editPost/page.tsx
+++ app/Marketing/MarketingPosts/editPost/page.tsx
@@ -3,17 +3,23 @@
 import HeaderEditMarketingPost from "../../Marketing/MarketingArticle/Add/headerEditPost";
 import EditMarketingPost from "../../Marketing/MarketingArticle/Add/editPost";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const EditPost: React.FC = () => {
   const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  const [id, setId] = useState<string | null>(null);
 
 
+  useEffect(() => {
+    const paramId = searchParams.get("id");
+    setId(paramId);
+  }, [searchParams]);
   return (
     <>
       <HeaderEditMarketingPost />
-      <EditMarketingPost id={`${id || ''}`} />
+      {id && (
+        <EditMarketingPost id={`${id}`} />
+      )}
     </>
   );
 };
