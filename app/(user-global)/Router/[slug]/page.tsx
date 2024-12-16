29adefb8b633313e21b9e4de25ba43f0beb34057
tuanhuynh0107 <ht24430@gmaile.com>
2024-12-16 15:10:42 +0700

    tuanhuynh merrge nhanh thuando

--- app/(user-global)/Router/[slug]/page.tsx
+++ app/(user-global)/Router/[slug]/page.tsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+
 const Router: React.FC<{ params: { id: string } }> = ({ params }) => {
+
+    const { id } = params
+    const [idRoute, setIdRoute] = useState<string>("");
+
+    const fetchIdCourse = async (id: string) => {
+        try {
+            const response = await fetch(`/api/slugById/${id}/Route`);
+
+            if (!response.ok) {
+                throw new Error(`API error: ${response.status}`);
+            }
+            const result = await response.json();
+            console.log(result)
+            setIdRoute(result.Route);
+        } catch (error: any) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (id) fetchIdCourse(id)
+    }, [id])
 
     return (
         <>Trang lộ trình</>
